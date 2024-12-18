9a6feaf359bd1f8d8dad4f7075a271681ef4df4f
Qurance <Qurance@foxmail.com>
2024-12-18 14:42:02 +0800

    Implement Form Validation

--- app/ui/invoices/edit-form.tsx
+++ app/ui/invoices/edit-form.tsx
@@ -1,7 +1,8 @@
 'use client';
 
 import { CustomerField, InvoiceForm } from '@/app/lib/definitions';
-import { updateInvoice } from '@/app/lib/actions';
+import { updateInvoice, State } from '@/app/lib/actions';
+import { useActionState } from 'react';
 import {
   CheckIcon,
   ClockIcon,
@@ -25,9 +26,11 @@ export default function EditInvoiceForm({
   // <form action={updateInvoice(id)}>
   // Instead, you can pass id to the Server Action using JS bind. This will ensure that any values passed to the Server Action are encoded.
 
+  const initialState: State = { message: null, errors: {} };
   const updateInvoiceWithId = updateInvoice.bind(null, invoice.id);
+  const [state, formAction] = useActionState(updateInvoiceWithId, initialState);
   return (
-    <form action={updateInvoiceWithId}>
+    <form action={formAction}>
       <div className="rounded-md bg-gray-50 p-4 md:p-6">
         {/* Customer Name */}
         <div className="mb-4">
